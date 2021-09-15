89df39fac8790fac44b4054ee5c86f1c6a1c15ff
Bruno Dantas <bruno.dantas@maxtool.com.br>
2021-09-15 11:01:30 -0300

    VARIAVEIS DE INSTACIA / SESSAO E QUERY PARAMS

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,7 @@
 class WelcomeController < ApplicationController
+
   def index
+    @nome = params[:my_name]
+    @curso = params[:my_course]
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,15 @@ module ApplicationHelper
     data_us.strftime("%d/%m/%Y")
   end
 
+  def whos_ambient_rails
+    if Rails.env.production?
+      "Production Ambient"
+    elsif Rails.env.development?
+      "Dev Ambient"
+    else
+      "Test Ambient"
+    end
+
+  end
+
 end
