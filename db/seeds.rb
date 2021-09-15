1d56a9b4d98ac7d02f00f087fb92e9db1881d59e
Bruno Dantas <bruno.dantas@maxtool.com.br>
2021-09-15 13:57:15 -0300

    RAILS TASK SEED TO DB MANIPULATE

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Coin.create!(
+  [
+    { description: "Bitcoin",
+      acronym: "BTC",
+      url_image: "",
+    },
+    { description: "Ethereum",
+      acronym: "ETH",
+      url_image: "",
+    },
+  ]
+)
