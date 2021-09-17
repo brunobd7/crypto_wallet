acf993fa877fd87c60450a091c6c54b60d961c34
Bruno Dantas <bruno.dantas@maxtool.com.br>
2021-09-17 14:08:47 -0300

    COOKIES AND SESSION WIHT RAILS

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,10 @@
 class WelcomeController < ApplicationController
 
   def index
+
+    cookies[:curso] = "Ruby on Rails - [COOKIE]"
+    session[:curso] = "Ruby on Rails - [SESSION]"
+
     @nome = params[:my_name]
     @curso = params[:my_course]
   end
